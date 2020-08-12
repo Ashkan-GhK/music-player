0bdb9b7f8b213b6281c177a959d838f279a349ec
Alex <alex.gh84@yahoo.co.uk>
2020-08-12 19:59:19 +0100

    navbar added

--- music-player-mern-stack-project/src/components/Navbar.js
+++ music-player-mern-stack-project/src/components/Navbar.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import TelegramIcon from '@material-ui/icons/Telegram';
+import '../styles/Navbar.css';
 
 const Navbar = () =>  {
     return (
         <div className="navbar_container">
             <div className="leftColumn_container">
                 <div className="socialMedias_container">
-                    <h5>f  t  in  Y  T</h5>
+                    <FacebookIcon fontSize= "small"/>
+                    <TwitterIcon  fontSize= "small" />
+                    <InstagramIcon fontSize= "small" />
+                    <YouTubeIcon fontSize= "small" />
+                    <TelegramIcon fontSize= "small" />
+    
                 </div>
                 <div className="search_container">
                     <SearchIcon />
                     <div className="search_input_container">
-                        <input type="text" placeholder="search" />
+                        <input className="search_box" type="text" placeholder="search" />
                     </div>
                 </div>
                 <div className="rightColumn_container">
-                    <h3>Listen Now</h3>
+                    <h3><span>Listen Now</span></h3>
                     <h3>Log In</h3>
                     <h3>Sign Up</h3>
 
