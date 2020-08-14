da09de7a6fa6b1015191250b55f6c48b1e538195
Alex <ashkan.ghk84@gmail.com>
2020-08-14 10:21:34 +0100

    header added

--- music-player-mern-stack-project/src/App.js
+++ music-player-mern-stack-project/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import BodyHeader from './components/BodyHeader';
+import BodyTop from './components/BodyTop';
+import BodyMiddle from './components/BodyMiddle';
+
 import './styles/App.css';
 
 function App() {
   return ( 
     <div className = "app" >
     <Navbar /> 
+    <BodyHeader />
+    <BodyTop />
+    <BodyMiddle />
+
     </div>
   );
 }
